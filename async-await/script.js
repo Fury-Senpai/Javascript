478145df7adf858b736f58ce788d272e1da0a742
Fury-Senpai <harbola03nitish@gmail.com>
2026-02-11 15:40:14 +0530

    async await with fetch

--- async-await/script.js
+++ async-await/script.js
@@ -34,3 +34,18 @@ async function abcd(){
 }
 
 abcd()
+
+// async await with fetch
+
+async function fetchData(){
+   try {
+     const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+     const data = await response.json();
+
+     console.log(data);
+   } catch (error) {
+    console.log(error);
+   }
+}
+
+fetchData()
